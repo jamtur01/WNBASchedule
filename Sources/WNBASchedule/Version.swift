81a3bd7f8261913b0b4c6ad49043df7fe87aa4c1
James Turnbulls <james@lovedthanlost.net>
2025-06-07 02:15:23 +0100

    Bump version to 1.2.0

--- Sources/WNBASchedule/Version.swift
+++ Sources/WNBASchedule/Version.swift
@@ -3,7 +3,7 @@ import Foundation
 /// Version information for the WNBASchedule app
 struct Version {
     /// Current version of the app
-    static let version = "1.1.0"
+    static let version = "1.2.0"
     
     /// Build number, typically incremented for each build
     static let build = "20250607"
